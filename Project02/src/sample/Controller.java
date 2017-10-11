d78bc06d8604964c9a708f7452f2a2a789a5a7a2
tangdavid <tang.david.2000@gmail.com>
2017-10-11 13:39:08 -0500

    Welcome Screen

--- Project02/src/sample/Controller.java
+++ Project02/src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Controller {
+    @FXML private Text actiontarget;
+
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+
+    public void createAttendance(ActionEvent actionEvent) {
+    }
+
+    public void createGrades(ActionEvent actionEvent) {
+    }
 }
