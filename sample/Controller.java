46992defbc57c74dd190c2e6a59bd496462d6370
tangdavid <tang.david.2000@gmail.com>
2017-10-11 14:19:16 -0500

    Welcome

--- sample/Controller.java
+++ sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML Button createGrades, createAttendance;
+
+    public void createAttendance(ActionEvent actionEvent) {
+
+    }
+
+    public void createGrades(ActionEvent actionEvent) {
+
+    }
 }
