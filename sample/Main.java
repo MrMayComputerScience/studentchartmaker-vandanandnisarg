ee158b9d4b43d0e3926fbfcb967eb6a8d263fb6d
tangdavid <tang.david.2000@gmail.com>
2017-10-13 14:23:06 -0500

    Attendance

--- sample/Main.java
+++ sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("attendance.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
--- sample/attendanceController.java
+++ sample/attendanceController.java
@@ -5,8 +5,114 @@ package sample;
  */
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 public class attendanceController {
+
+    final FileChooser fileLoader = new FileChooser();
+    File studentFile, datesFile, headerFile;
+
+    private ArrayList<String> studentList, datesList, headerList, studentFinal;
+
+    @FXML Button loadStudentButton;
+    @FXML Text displayStudentFile, displayHeaderFile, displayDatesFile;
+
+    public void loadStudents(ActionEvent actionEvent) {
+        Scene stage = loadStudentButton.getScene();
+        fileLoader.setTitle("Choose Student File");
+
+        fileLoader.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        studentFile = fileLoader.showOpenDialog(stage.getWindow());
+        System.out.println(studentFile.getName());
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(studentFile));
+            studentList = new ArrayList<String>();
+            studentFinal = new ArrayList<>();
+
+            String student;
+            while((student = reader.readLine()) != null){
+                studentList.add(student);
+            }
+    }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        //Dumb way to truncate data
+
+        for (String student: studentList) {
+            String[] clean = student.split("  ");
+            String[] temp = clean[0].split(", ");
+            studentFinal.add(temp[1]+" "+temp[0]);
+    }
+
+        displayStudentFile.setText(studentFile.getName());
+    }
+
+    public void loadDates(ActionEvent actionEvent) {
+        Scene stage = loadStudentButton.getScene();
+        fileLoader.setTitle("Choose Dates File");
+
+        fileLoader.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        datesFile = fileLoader.showOpenDialog(stage.getWindow());
+        System.out.println(datesFile.getName());
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(datesFile));
+            datesList = new ArrayList<String>();
+
+            String date;
+            while((date = reader.readLine()) != null){
+                datesList.add(date);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        displayDatesFile.setText(datesFile.getName());
+    }
+
+    public void loadHeader(ActionEvent actionEvent) {
+        Scene stage = loadStudentButton.getScene();
+        fileLoader.setTitle("Choose Header File");
+
+        fileLoader.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+        headerFile = fileLoader.showOpenDialog(stage.getWindow());
+        System.out.println(headerFile.getName());
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(headerFile));
+            headerList = new ArrayList<String>();
+
+            String header;
+            while((header = reader.readLine()) != null){
+                headerList.add(header);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        displayHeaderFile.setText(headerFile.getName());
+    }
+
+    public void createChart(ActionEvent actionEvent) {
+    }
 }
