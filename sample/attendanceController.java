e6cf0fef350798c98f4b0782c8aaed931612fb92
tangdavid <tang.david.2000@gmail.com>
2017-10-26 13:41:17 -0500

    Fix Alpha

--- sample/attendanceController.java
+++ sample/attendanceController.java
@@ -100,7 +100,6 @@ public class attendanceController {
         for (String student: studentFinal){
             System.out.println(student);
         }
-        Collections.sort(studentFinal);
     }
 
     public void loadDates(ActionEvent actionEvent) {
